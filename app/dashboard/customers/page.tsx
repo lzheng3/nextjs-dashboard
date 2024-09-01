2b4f2ad0010806dde748b016cf6e898d5bb12241
lzheng3 <lekaizheng@icloud.com>
2024-08-31 23:04:34 -0400

    fix import error

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomerPage = () => {
+  return <div>Customer page</div>;
+};
+
+export default CustomerPage;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Dash page</div>;
+};
+
+export default page;
